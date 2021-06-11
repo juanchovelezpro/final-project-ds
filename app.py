2b2feaca48ea4a26b26d4ed8e18ce881b0c25017
juanchovelezpro <velezolaya2012@hotmail.com>
2021-06-11 00:20:07 -0500

    Changing context of the app

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    count = redis.incr('seen')
+    return 'You have seen this service deployed in a kubernetes cluster {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
